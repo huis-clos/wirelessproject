68a203146ceda98cae8d34f04b17969a11fe9496
Colleen Toth <tothcol@pdx.edu>
2015-05-24 15:38:35 -0700

    added .gitignore

---
+++ testchan1/packet_sniffer.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+#Python script to parse tcpDump file and get out 
+#signal strength while also formatting. Reads in a
+#tcpdump that has been written to a file and writes
+#out desired output to a new file
+
+#open file
+import sys
+
+
+def main(argv):
+    sigList = []
+
+    print "\n\n********** Begin parsing file for signal strength *********\n\n"
+
+    count = 0
+    countTA = 0
+    print argv
+    for item in argv:
+        aFile = open(item, 'r')
+        for line in aFile:
+            aLine = line.split()
+            for i in aLine:
+                if 'DA' in i.split(':'):
+                    if 'dB' in aLine[8]:
+                        aWord = aLine[8].strip('dB')
+                        aWord = int(aWord)
+                        sigList.append(aWord)
+                countTA = countTA + 1
+            count = count + 1
+
+        aFile.close()
+
+    print sigList
+    print len(sigList)
+    print count
+    print countTA
+
+    print "\n\n*********** Writing to file ***********\n\n"
+
+    tcpOutput = open('output' + argv[0], 'w')
+
+    for i in sigList:
+        tcpOutput.write("%s\n" % i)
+
+    tcpOutput.close()
+
+    print "\n\n*********** Done **************\n\n"
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+ 
