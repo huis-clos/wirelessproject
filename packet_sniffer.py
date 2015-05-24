74961648a8811eb47cc6ee2059340d22ef343e11
Colleen Toth <tothcol@pdx.edu>
2015-05-24 15:55:07 -0700

    added script to chan11 directory

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 #Python script to parse tcpDump file and get out 
 #signal strength while also formatting. Reads in a
 #tcpdump that has been written to a file and writes
@@ -5,43 +6,51 @@
 
 #open file
 
-tcpFile = open('tcpDump', 'r')
-
-sigList = []
-
-print "\n\n********** Begin parsing file for signal strength *********\n\n"
-
-count = 0
-countTA = 0
-
-for line in tcpFile:
-	aLine = line.split()
-	for i in aLine:
-		if 'TA' in i.split(':'):
-			if 'dB' in aLine[8]:
-				aWord = aLine[8].strip('dB')
-				aWord = int(aWord)
-				sigList.append(aWord)
-			countTA = countTA + 1
-	count = count + 1
+def main(argv):
 
 
-tcpFile.close()
+	sigList = []
 
-print sigList
-print len(sigList)
-print count
-print countTA
+	print "\n\n********** Begin parsing file for signal strength *********\n\n"
+
+	count = 0
+	countTA = 0
+	len = len(argv)
+	for item in argv:
+		aFile = open(item, 'r')
+		for line in aFile:
+			aLine = line.split()
+				for i in aLine:
+					if 'TA' in i.split(':'):
+						if 'dB' in aLine[8]:
+							aWord = aLine[8].strip('dB')
+							aWord = int(aWord)
+							sigList.append(aWord)
+					countTA = countTA + 1
+				count = count + 1
 
 
-print "\n\n*********** Writing to file ***********\n\n"
+		aFile.close()
 
-tcpOutput = open('output', 'w')
+	print sigList
+	print len(sigList)
+	print count
+	print countTA
 
-for i in sigList:
-	tcpOutput.write("%s\n" % i)
 
-tcpOutput.close()
+	print "\n\n*********** Writing to file ***********\n\n"
 
-print "\n\n*********** Done **************\n\n"
+	tcpOutput = open('output'+ argv[0], 'w')
+
+	for i in sigList:
+		tcpOutput.write("%s\n" % i)
+
+	tcpOutput.close()
+
+	print "\n\n*********** Done **************\n\n"
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:0])
+
  
